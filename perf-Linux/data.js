c9b0a6faef018c187e95388c05549ffe0516485b
github-action-benchmark <github@users.noreply.github.com>
2024-10-06 19:28:48 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 36b36505a4dcf83b76846432d88ca7baeb2e43da

---
+++ perf-Linux/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1728242928777,
+  "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jayree.git@icloud.com",
+            "name": "jayree",
+            "username": "jayree"
+          },
+          "committer": {
+            "email": "jayree.git@icloud.com",
+            "name": "jayree",
+            "username": "jayree"
+          },
+          "distinct": true,
+          "id": "36b36505a4dcf83b76846432d88ca7baeb2e43da",
+          "message": "chore: add perfScaleNut workflow",
+          "timestamp": "2024-10-06T21:27:41+02:00",
+          "tree_id": "36300ced03f8e81513e75c3aba1760991bc5c5cc",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/36b36505a4dcf83b76846432d88ca7baeb2e43da"
+        },
+        "date": 1728242928249,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-fromGitDiff-linux",
+            "value": 3053,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
