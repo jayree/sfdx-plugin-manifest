fff9224f603b56f0cc4f87bf290500c7c1846f5c
github-action-benchmark <github@users.noreply.github.com>
2024-10-06 20:43:00 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 36237f1361b3615887d5c3e4b15cc20289015f8e

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728246753823,
+  "lastUpdate": 1728247380219,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -173,6 +173,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-fromGitDiff-win32",
             "value": 5255,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "36237f1361b3615887d5c3e4b15cc20289015f8e",
+          "message": "chore(deps-dev): bump oclif from 4.15.2 to 4.15.5\n\nBumps [oclif](https://github.com/oclif/oclif) from 4.15.2 to 4.15.5.\n- [Release notes](https://github.com/oclif/oclif/releases)\n- [Changelog](https://github.com/oclif/oclif/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/oclif/oclif/compare/4.15.2...4.15.5)\n\n---\nupdated-dependencies:\n- dependency-name: oclif\n  dependency-type: direct:development\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2024-10-06T20:40:12Z",
+          "tree_id": "a8be4c5fdd185584b72f40a9b0ba75a0e999a337",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/36237f1361b3615887d5c3e4b15cc20289015f8e"
+        },
+        "date": 1728247373452,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-fromGitDiff-win32",
+            "value": 5521,
             "unit": "ms"
           }
         ]
