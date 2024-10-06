726eb73b8160e83fc74bb4486086a7c1251c50a0
github-action-benchmark <github@users.noreply.github.com>
2024-10-06 19:34:08 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 46db7e0fac960996c4fe10f7e4dd422bf0d046e6

---
+++ perf-Windows/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1728243248067,
+  "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jayree.git@icloud.com",
+            "name": "jayree",
+            "username": "jayree"
+          },
+          "committer": {
+            "email": "jayree.git@icloud.com",
+            "name": "jayree",
+            "username": "jayree"
+          },
+          "distinct": true,
+          "id": "46db7e0fac960996c4fe10f7e4dd422bf0d046e6",
+          "message": "chore: add perfScaleNut workflow",
+          "timestamp": "2024-10-06T21:30:49+02:00",
+          "tree_id": "f4a7858ceabad6e2e4a539ce9d6583760b2b0e40",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/46db7e0fac960996c4fe10f7e4dd422bf0d046e6"
+        },
+        "date": 1728243240910,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-fromGitDiff-win32",
+            "value": 5556,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
