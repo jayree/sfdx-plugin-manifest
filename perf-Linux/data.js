b822f08fabe50d849068be4d26f61ca06386904d
github-action-benchmark <github@users.noreply.github.com>
2024-12-14 12:17:58 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 6f01ff2fb7e33f7f569ccf64d506eb2261222970

--- perf-Linux/data.js
+++ perf-Linux/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1734178294438,
+  "lastUpdate": 1734178678524,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -6511,6 +6511,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-linux",
             "value": 247,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6f01ff2fb7e33f7f569ccf64d506eb2261222970",
+          "message": "chore(deps-dev): bump @oclif/plugin-help from 6.2.18 to 6.2.19\n\nBumps [@oclif/plugin-help](https://github.com/oclif/plugin-help) from 6.2.18 to 6.2.19.\n- [Release notes](https://github.com/oclif/plugin-help/releases)\n- [Changelog](https://github.com/oclif/plugin-help/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/oclif/plugin-help/compare/6.2.18...6.2.19)\n\n---\nupdated-dependencies:\n- dependency-name: \"@oclif/plugin-help\"\n  dependency-type: direct:development\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2024-12-14T12:13:11Z",
+          "tree_id": "18b3c3289c34bbee2b11dda69e8b2f8004df54bf",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/6f01ff2fb7e33f7f569ccf64d506eb2261222970"
+        },
+        "date": 1734178677615,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-linux",
+            "value": 2656,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-linux",
+            "value": 1746,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-linux",
+            "value": 329,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-linux",
+            "value": 250,
             "unit": "ms"
           }
         ]
