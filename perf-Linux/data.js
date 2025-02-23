02403f5f87d6e53f261ff087154d96272148df44
github-action-benchmark <github@users.noreply.github.com>
2025-02-23 12:28:27 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 38f471a2e4e9346bb6e60a2e06dee5cca74fec87

--- perf-Linux/data.js
+++ perf-Linux/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740313364372,
+  "lastUpdate": 1740313707002,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -13155,6 +13155,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-linux",
             "value": 249,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "38f471a2e4e9346bb6e60a2e06dee5cca74fec87",
+          "message": "fix(deps): bump @jayree/changelog from 1.2.5 to 1.2.6\n\nBumps [@jayree/changelog](https://github.com/jayree/changelog) from 1.2.5 to 1.2.6.\n- [Release notes](https://github.com/jayree/changelog/releases)\n- [Changelog](https://github.com/jayree/changelog/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/jayree/changelog/compare/v1.2.5...v1.2.6)\n\n---\nupdated-dependencies:\n- dependency-name: \"@jayree/changelog\"\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2025-02-23T12:22:12Z",
+          "tree_id": "3b5ca9c83bff1fb6b1d0d091890cdca22bc60868",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/38f471a2e4e9346bb6e60a2e06dee5cca74fec87"
+        },
+        "date": 1740313705430,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-linux",
+            "value": 2799,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-linux",
+            "value": 1864,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-linux",
+            "value": 291,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-linux",
+            "value": 264,
             "unit": "ms"
           }
         ]
