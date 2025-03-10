d2cd660aa73051ff6d55db36df7ce2034f3086a3
github-action-benchmark <github@users.noreply.github.com>
2025-03-10 09:10:03 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for f0666f863ecce1087a7859d2517e215b820ca117

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741597506121,
+  "lastUpdate": 1741597803664,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -14915,6 +14915,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-win32",
             "value": 393,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f0666f863ecce1087a7859d2517e215b820ca117",
+          "message": "fix(deps): bump @oclif/core from 4.2.8 to 4.2.9\n\nBumps [@oclif/core](https://github.com/oclif/core) from 4.2.8 to 4.2.9.\n- [Release notes](https://github.com/oclif/core/releases)\n- [Changelog](https://github.com/oclif/core/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/oclif/core/compare/4.2.8...4.2.9)\n\n---\nupdated-dependencies:\n- dependency-name: \"@oclif/core\"\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2025-03-10T09:07:27Z",
+          "tree_id": "6400e6b5bca251c49e8f5d8e24db6b30b4675675",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/f0666f863ecce1087a7859d2517e215b820ca117"
+        },
+        "date": 1741597797958,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-win32",
+            "value": 5270,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-win32",
+            "value": 3638,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-win32",
+            "value": 390,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-win32",
+            "value": 337,
             "unit": "ms"
           }
         ]
