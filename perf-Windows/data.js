09d66d65d86ef06d9f1263bea38f2f5f277b7ec2
github-action-benchmark <github@users.noreply.github.com>
2025-03-29 15:55:38 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 000226ab52c19ceb1eba3bfe7bae73b3954a064d

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743263197704,
+  "lastUpdate": 1743263738068,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -17159,6 +17159,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-win32",
             "value": 344,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "000226ab52c19ceb1eba3bfe7bae73b3954a064d",
+          "message": "chore(deps): bump TriPSs/conventional-changelog-action\n\nBumps [TriPSs/conventional-changelog-action](https://github.com/tripss/conventional-changelog-action) from 5.3.0 to 6.0.0.\n- [Release notes](https://github.com/tripss/conventional-changelog-action/releases)\n- [Changelog](https://github.com/TriPSs/conventional-changelog-action/blob/releases/v6/CHANGELOG.md)\n- [Commits](https://github.com/tripss/conventional-changelog-action/compare/3a392e9aa44a72686b0fc13259a90d287dd0877c...67139193614f5b9e8db87da1bd4240922b34d765)\n\n---\nupdated-dependencies:\n- dependency-name: TriPSs/conventional-changelog-action\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2025-03-29T15:51:53Z",
+          "tree_id": "03a6155fffacdaf342c63dc84556e9c3f22bfb96",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/000226ab52c19ceb1eba3bfe7bae73b3954a064d"
+        },
+        "date": 1743263720091,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-win32",
+            "value": 5810,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-win32",
+            "value": 3892,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-win32",
+            "value": 413,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-win32",
+            "value": 380,
             "unit": "ms"
           }
         ]
