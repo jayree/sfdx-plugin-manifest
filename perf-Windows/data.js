088606d571ce7af71d43aef35daee713cc3f19dd
github-action-benchmark <github@users.noreply.github.com>
2025-05-08 12:02:12 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 9daf02e7365c8a3c97b74e62d88b0458fb8f6263

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746704881501,
+  "lastUpdate": 1746705732706,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -21647,6 +21647,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-win32",
             "value": 346,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9daf02e7365c8a3c97b74e62d88b0458fb8f6263",
+          "message": "fix(deps): bump @jayree/changelog from 1.2.8 to 1.2.9\n\nBumps [@jayree/changelog](https://github.com/jayree/changelog) from 1.2.8 to 1.2.9.\n- [Release notes](https://github.com/jayree/changelog/releases)\n- [Changelog](https://github.com/jayree/changelog/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/jayree/changelog/compare/v1.2.8...v1.2.9)\n\n---\nupdated-dependencies:\n- dependency-name: \"@jayree/changelog\"\n  dependency-version: 1.2.9\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2025-05-08T11:56:29Z",
+          "tree_id": "37b1e96ce69845064e57603b9830f5c1cbcecc31",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/9daf02e7365c8a3c97b74e62d88b0458fb8f6263"
+        },
+        "date": 1746705716012,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-win32",
+            "value": 5343,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-win32",
+            "value": 3495,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-win32",
+            "value": 377,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-win32",
+            "value": 331,
             "unit": "ms"
           }
         ]
