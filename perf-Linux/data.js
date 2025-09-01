5a1bc523a7007b9d05bc4dce20012e27c13fa0f5
github-action-benchmark <github@users.noreply.github.com>
2025-09-01 08:38:34 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for badfa95e27210e5a489f9366848f4cd52b383477

--- perf-Linux/data.js
+++ perf-Linux/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1756715744100,
+  "lastUpdate": 1756715914649,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -23007,6 +23007,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-linux",
             "value": 251,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "badfa95e27210e5a489f9366848f4cd52b383477",
+          "message": "chore(deps-dev): bump @salesforce/dev-scripts from 11.0.3 to 11.0.4\n\nBumps [@salesforce/dev-scripts](https://github.com/forcedotcom/dev-scripts) from 11.0.3 to 11.0.4.\n- [Release notes](https://github.com/forcedotcom/dev-scripts/releases)\n- [Changelog](https://github.com/forcedotcom/dev-scripts/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/forcedotcom/dev-scripts/compare/11.0.3...11.0.4)\n\n---\nupdated-dependencies:\n- dependency-name: \"@salesforce/dev-scripts\"\n  dependency-version: 11.0.4\n  dependency-type: direct:development\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2025-09-01T08:36:05Z",
+          "tree_id": "ff9e942db53ac0c784b146ff8c36d23d8ca1d9b3",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/badfa95e27210e5a489f9366848f4cd52b383477"
+        },
+        "date": 1756715912407,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-linux",
+            "value": 2816,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-linux",
+            "value": 1874,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-linux",
+            "value": 354,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-linux",
+            "value": 240,
             "unit": "ms"
           }
         ]
