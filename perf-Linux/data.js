1b1b348803442a6f9477dcf9a1c966071fe8f44c
github-action-benchmark <github@users.noreply.github.com>
2025-12-28 15:16:08 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for c47a24e102c1653887751b0b9ab47049244b1455

--- perf-Linux/data.js
+++ perf-Linux/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766934317272,
+  "lastUpdate": 1766934967948,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -32330,6 +32330,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-VirtualTreeContainerExtra-linux",
             "value": 352,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-linux",
+            "value": 233,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c47a24e102c1653887751b0b9ab47049244b1455",
+          "message": "chore(deps-dev): bump oclif from 4.22.59 to 4.22.61\n\nBumps [oclif](https://github.com/oclif/oclif) from 4.22.59 to 4.22.61.\n- [Release notes](https://github.com/oclif/oclif/releases)\n- [Changelog](https://github.com/oclif/oclif/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/oclif/oclif/compare/4.22.59...4.22.61)\n\n---\nupdated-dependencies:\n- dependency-name: oclif\n  dependency-version: 4.22.61\n  dependency-type: direct:development\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2025-12-28T15:15:13Z",
+          "tree_id": "cab53a3c37ef64cc7c90831bc7667f2cef53d61c",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/c47a24e102c1653887751b0b9ab47049244b1455"
+        },
+        "date": 1766934963613,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-linux",
+            "value": 2269,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-linux",
+            "value": 1589,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-linux",
+            "value": 411,
             "unit": "ms"
           },
           {
