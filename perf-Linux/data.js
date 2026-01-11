66c8ed611efd4297232c8199b212bd3de8395936
github-action-benchmark <github@users.noreply.github.com>
2026-01-11 15:55:47 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 895505d89a24e0783e3dd410abdf63d7e84fd4aa

--- perf-Linux/data.js
+++ perf-Linux/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768035692259,
+  "lastUpdate": 1768146947806,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -32687,6 +32687,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-linux",
             "value": 249,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "895505d89a24e0783e3dd410abdf63d7e84fd4aa",
+          "message": "chore(deps-dev): bump oclif from 4.22.63 to 4.22.65\n\nBumps [oclif](https://github.com/oclif/oclif) from 4.22.63 to 4.22.65.\n- [Release notes](https://github.com/oclif/oclif/releases)\n- [Changelog](https://github.com/oclif/oclif/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/oclif/oclif/compare/4.22.63...4.22.65)\n\n---\nupdated-dependencies:\n- dependency-name: oclif\n  dependency-version: 4.22.65\n  dependency-type: direct:development\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-01-11T15:54:51Z",
+          "tree_id": "b584a161f465f936f0107b44bed11017d5c03708",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/895505d89a24e0783e3dd410abdf63d7e84fd4aa"
+        },
+        "date": 1768146944734,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-linux",
+            "value": 2596,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-linux",
+            "value": 1974,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-linux",
+            "value": 299,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-linux",
+            "value": 255,
             "unit": "ms"
           }
         ]
