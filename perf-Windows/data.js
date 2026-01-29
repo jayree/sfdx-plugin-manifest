eea285b7cf43bc61c70d2e65d9405c986014855c
github-action-benchmark <github@users.noreply.github.com>
2026-01-29 21:04:31 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 5e2eb8acaaaa5b12c4db8329741fd08b1f9bfa80

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1769719991830,
+  "lastUpdate": 1769720670900,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -33963,6 +33963,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-win32",
             "value": 381,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5e2eb8acaaaa5b12c4db8329741fd08b1f9bfa80",
+          "message": "fix(deps): bump isomorphic-git from 1.36.2 to 1.36.3\n\nBumps [isomorphic-git](https://github.com/isomorphic-git/isomorphic-git) from 1.36.2 to 1.36.3.\n- [Release notes](https://github.com/isomorphic-git/isomorphic-git/releases)\n- [Commits](https://github.com/isomorphic-git/isomorphic-git/compare/v1.36.2...v1.36.3)\n\n---\nupdated-dependencies:\n- dependency-name: isomorphic-git\n  dependency-version: 1.36.3\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-01-29T21:00:29Z",
+          "tree_id": "f85aba21d2d195446c2c70b98eca7438fd4830e0",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/5e2eb8acaaaa5b12c4db8329741fd08b1f9bfa80"
+        },
+        "date": 1769720649073,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-win32",
+            "value": 4630,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-win32",
+            "value": 3307,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-win32",
+            "value": 456,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-win32",
+            "value": 355,
             "unit": "ms"
           }
         ]
