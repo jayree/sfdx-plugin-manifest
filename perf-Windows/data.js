c2332f62d3c08a290171a336e65884f0543c79ff
github-action-benchmark <github@users.noreply.github.com>
2026-02-28 15:34:21 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 0db11bb0c61d2f10bafee9ccbfe30b240fbd18b6

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772103716369,
+  "lastUpdate": 1772292860927,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -36339,6 +36339,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-win32",
             "value": 409,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0db11bb0c61d2f10bafee9ccbfe30b240fbd18b6",
+          "message": "chore(deps): bump peter-evans/create-pull-request from 7 to 8\n\nBumps [peter-evans/create-pull-request](https://github.com/peter-evans/create-pull-request) from 7 to 8.\n- [Release notes](https://github.com/peter-evans/create-pull-request/releases)\n- [Commits](https://github.com/peter-evans/create-pull-request/compare/v7...v8)\n\n---\nupdated-dependencies:\n- dependency-name: peter-evans/create-pull-request\n  dependency-version: '8'\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-02-28T15:32:14Z",
+          "tree_id": "bf7c097a2e16703db69938589077c975f70708a0",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/0db11bb0c61d2f10bafee9ccbfe30b240fbd18b6"
+        },
+        "date": 1772292854605,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-win32",
+            "value": 4880,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-win32",
+            "value": 3298,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-win32",
+            "value": 523,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-win32",
+            "value": 405,
             "unit": "ms"
           }
         ]
