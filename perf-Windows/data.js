d8ac069b211436648f946dccb7a9d850ecba38db
github-action-benchmark <github@users.noreply.github.com>
2026-04-02 17:10:01 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 05f41ec1ac74cbc1ac26187052ea43daa6a29bd6

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775148061100,
+  "lastUpdate": 1775149801509,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -40915,6 +40915,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-win32",
             "value": 408,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "05f41ec1ac74cbc1ac26187052ea43daa6a29bd6",
+          "message": "fix(deps): bump lodash from 4.17.23 to 4.18.1\n\nBumps [lodash](https://github.com/lodash/lodash) from 4.17.23 to 4.18.1.\n- [Release notes](https://github.com/lodash/lodash/releases)\n- [Commits](https://github.com/lodash/lodash/compare/4.17.23...4.18.1)\n\n---\nupdated-dependencies:\n- dependency-name: lodash\n  dependency-version: 4.18.1\n  dependency-type: indirect\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-04-02T17:07:37Z",
+          "tree_id": "b9e3a3a348b4522a91b1bd9506676bc5f1a6a5d8",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/05f41ec1ac74cbc1ac26187052ea43daa6a29bd6"
+        },
+        "date": 1775149794678,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-win32",
+            "value": 5136,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-win32",
+            "value": 3426,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-win32",
+            "value": 562,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-win32",
+            "value": 411,
             "unit": "ms"
           }
         ]
