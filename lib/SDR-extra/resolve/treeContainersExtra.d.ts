caaecaf2907a9dd4489a9bbba5a761d95a78c7b0
jayree <jayree.git@icloud.com>
2026-04-12 17:23:16 +0200

    fix: proper directory ignoring

--- lib/SDR-extra/resolve/treeContainersExtra.d.ts
+++ lib/SDR-extra/resolve/treeContainersExtra.d.ts
@@ -1,8 +1,17 @@
 import { VirtualTreeContainer } from '@salesforce/source-deploy-retrieve';
 export declare class VirtualTreeContainerExtra extends VirtualTreeContainer {
     /**
+     * Designed for recreating virtual files from file paths and their buffer content
+     * This was created to support use of MetadataResolver with git diff results where the modified files and their content can be provided but the files don't actually exist on the filesystem
+     *
+     * @param paths full paths to files
+     * @param fileBufferByPath map of file paths to their buffer content
+     * @returns VirtualTreeContainer
+     */
+    static fromFilePathsWithBlobs(paths: string[], fileBufferByPath: Map<string, Buffer>): VirtualTreeContainer;
+    /**
      * Designed for recreating virtual files from a git ref
-     * To support use of MetadataResolver to also resolve metadata xmls file names can be provided
+     * This was created to support use of MetadataResolver with git diff results where the modified files and their content can be provided but the files don't actually exist on the filesystem
      *
      * @param ref git ref
      * @param dir git dir
