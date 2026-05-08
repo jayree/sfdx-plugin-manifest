4cf2a81ff2f3bb889b65466c07424b9958351e48
github-action-benchmark <github@users.noreply.github.com>
2026-05-08 23:17:59 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 4fa23bf6e822dee0fff5bf6e7fb88333c09c9c0b

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778281641571,
+  "lastUpdate": 1778282279064,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -45183,6 +45183,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-win32",
             "value": 432,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4fa23bf6e822dee0fff5bf6e7fb88333c09c9c0b",
+          "message": "fix(deps): bump fast-xml-parser from 5.7.2 to 5.7.3\n\nBumps [fast-xml-parser](https://github.com/NaturalIntelligence/fast-xml-parser) from 5.7.2 to 5.7.3.\n- [Release notes](https://github.com/NaturalIntelligence/fast-xml-parser/releases)\n- [Changelog](https://github.com/NaturalIntelligence/fast-xml-parser/blob/master/CHANGELOG.md)\n- [Commits](https://github.com/NaturalIntelligence/fast-xml-parser/compare/v5.7.2...v5.7.3)\n\n---\nupdated-dependencies:\n- dependency-name: fast-xml-parser\n  dependency-version: 5.7.3\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-05-08T23:15:52Z",
+          "tree_id": "05010daa4ce11729983777af9939c8f2b42e001d",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/4fa23bf6e822dee0fff5bf6e7fb88333c09c9c0b"
+        },
+        "date": 1778282272444,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-win32",
+            "value": 3650,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-win32",
+            "value": 2049,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-win32",
+            "value": 351,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-win32",
+            "value": 421,
             "unit": "ms"
           }
         ]
