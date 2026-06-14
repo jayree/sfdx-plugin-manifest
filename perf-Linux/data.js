746244a06120b3b7c59080ce76848e417ce6565c
github-action-benchmark <github@users.noreply.github.com>
2026-06-14 16:31:55 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 2825813606ff6b6cf49a4fe1672ba88fae9b993f

--- perf-Linux/data.js
+++ perf-Linux/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781454015354,
+  "lastUpdate": 1781454715420,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -49055,6 +49055,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-linux",
             "value": 244,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2825813606ff6b6cf49a4fe1672ba88fae9b993f",
+          "message": "chore(deps-dev): bump prettier from 3.8.3 to 3.8.4\n\nBumps [prettier](https://github.com/prettier/prettier) from 3.8.3 to 3.8.4.\n- [Release notes](https://github.com/prettier/prettier/releases)\n- [Changelog](https://github.com/prettier/prettier/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/prettier/prettier/compare/3.8.3...3.8.4)\n\n---\nupdated-dependencies:\n- dependency-name: prettier\n  dependency-version: 3.8.4\n  dependency-type: direct:development\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-06-14T16:30:57Z",
+          "tree_id": "e29fdfa651632177353a60a19a8bd58bb5e241f4",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/2825813606ff6b6cf49a4fe1672ba88fae9b993f"
+        },
+        "date": 1781454709915,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-linux",
+            "value": 2018,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-linux",
+            "value": 1227,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-linux",
+            "value": 285,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-linux",
+            "value": 363,
             "unit": "ms"
           }
         ]
