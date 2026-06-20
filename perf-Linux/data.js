7f2d9d411cc719ee45685fe81a8b5e6109895d83
github-action-benchmark <github@users.noreply.github.com>
2026-06-20 11:11:51 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for f3b7850d8eda93121b8c646a4ca0d338d27b1d87

--- perf-Linux/data.js
+++ perf-Linux/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781953633511,
+  "lastUpdate": 1781953911921,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -49715,6 +49715,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-linux",
             "value": 331,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f3b7850d8eda93121b8c646a4ca0d338d27b1d87",
+          "message": "fix(deps): bump isomorphic-git from 1.38.4 to 1.38.5\n\nBumps [isomorphic-git](https://github.com/isomorphic-git/isomorphic-git) from 1.38.4 to 1.38.5.\n- [Release notes](https://github.com/isomorphic-git/isomorphic-git/releases)\n- [Commits](https://github.com/isomorphic-git/isomorphic-git/compare/v1.38.4...v1.38.5)\n\n---\nupdated-dependencies:\n- dependency-name: isomorphic-git\n  dependency-version: 1.38.5\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-06-20T11:06:32Z",
+          "tree_id": "30e860b5476f083b1a024859036860982af741f0",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/f3b7850d8eda93121b8c646a4ca0d338d27b1d87"
+        },
+        "date": 1781953906640,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-linux",
+            "value": 1958,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-linux",
+            "value": 1221,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-linux",
+            "value": 244,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-linux",
+            "value": 317,
             "unit": "ms"
           }
         ]
