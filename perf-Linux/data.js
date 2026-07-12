b44a659e767992147433332d851d3eaddd3a5325
github-action-benchmark <github@users.noreply.github.com>
2026-07-12 16:44:06 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 885ed6a746087a993e412e004897493c7e93cf5e

--- perf-Linux/data.js
+++ perf-Linux/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1783873938148,
+  "lastUpdate": 1783874646558,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -52399,6 +52399,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-linux",
             "value": 333,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "885ed6a746087a993e412e004897493c7e93cf5e",
+          "message": "fix(deps): bump @salesforce/core from 8.32.0 to 8.32.2\n\nBumps [@salesforce/core](https://github.com/forcedotcom/sfdx-core) from 8.32.0 to 8.32.2.\n- [Release notes](https://github.com/forcedotcom/sfdx-core/releases)\n- [Changelog](https://github.com/forcedotcom/sfdx-core/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/forcedotcom/sfdx-core/compare/8.32.0...8.32.2)\n\n---\nupdated-dependencies:\n- dependency-name: \"@salesforce/core\"\n  dependency-version: 8.32.2\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-07-12T16:43:10Z",
+          "tree_id": "e437bfa9a0c772f5138b7128b17444b3ea910b22",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/885ed6a746087a993e412e004897493c7e93cf5e"
+        },
+        "date": 1783874640594,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-linux",
+            "value": 2138,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-linux",
+            "value": 1338,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-linux",
+            "value": 348,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-linux",
+            "value": 465,
             "unit": "ms"
           }
         ]
