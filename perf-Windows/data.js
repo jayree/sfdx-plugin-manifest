2cffe38e58202e6a6437e9203240a56da91d0ef8
github-action-benchmark <github@users.noreply.github.com>
2026-07-16 01:05:23 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 7973bdddffa585cb6d4358fbb237c2e47ba469f3

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1783874709321,
+  "lastUpdate": 1784163923067,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -52399,6 +52399,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-win32",
             "value": 505,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7973bdddffa585cb6d4358fbb237c2e47ba469f3",
+          "message": "fix(deps): bump websocket-driver from 0.7.4 to 0.7.5\n\nBumps [websocket-driver](https://github.com/faye/websocket-driver-node) from 0.7.4 to 0.7.5.\n- [Changelog](https://github.com/faye/websocket-driver-node/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/faye/websocket-driver-node/compare/0.7.4...0.7.5)\n\n---\nupdated-dependencies:\n- dependency-name: websocket-driver\n  dependency-version: 0.7.5\n  dependency-type: indirect\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-07-16T01:03:23Z",
+          "tree_id": "070b2a89b0b8f82c668f1a3aaafefce808259364",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/7973bdddffa585cb6d4358fbb237c2e47ba469f3"
+        },
+        "date": 1784163915704,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-win32",
+            "value": 3008,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-win32",
+            "value": 1767,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-win32",
+            "value": 346,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-win32",
+            "value": 353,
             "unit": "ms"
           }
         ]
