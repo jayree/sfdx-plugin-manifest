e54853823a3b8fa8bdd93302ce430426ca202e4c
github-action-benchmark <github@users.noreply.github.com>
2026-07-19 17:17:15 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 86afe00ae353ed50c7c371e08c1760fdfeff12f7

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784480790574,
+  "lastUpdate": 1784481434871,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -53098,6 +53098,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-VirtualTreeContainerExtra-win32",
             "value": 388,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-win32",
+            "value": 480,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "86afe00ae353ed50c7c371e08c1760fdfeff12f7",
+          "message": "chore(deps-dev): bump c8 from 11.0.0 to 12.0.0\n\nBumps [c8](https://github.com/bcoe/c8) from 11.0.0 to 12.0.0.\n- [Release notes](https://github.com/bcoe/c8/releases)\n- [Changelog](https://github.com/bcoe/c8/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/bcoe/c8/compare/v11.0.0...v12.0.0)\n\n---\nupdated-dependencies:\n- dependency-name: c8\n  dependency-version: 12.0.0\n  dependency-type: direct:development\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-07-19T17:14:55Z",
+          "tree_id": "eea619742bbd7daa6c9b1cb13382f03a2404b117",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/86afe00ae353ed50c7c371e08c1760fdfeff12f7"
+        },
+        "date": 1784481427167,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-win32",
+            "value": 4196,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-win32",
+            "value": 2508,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-win32",
+            "value": 462,
             "unit": "ms"
           },
           {
