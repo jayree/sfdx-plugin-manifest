ca3b3ab2abb76f6d14a077b8c45a05a401d8887a
github-action-benchmark <github@users.noreply.github.com>
2026-07-24 10:03:04 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for d034cbd7ccaf7b073adc980582a9299dedf07854

--- perf-Linux/data.js
+++ perf-Linux/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784886684739,
+  "lastUpdate": 1784887384139,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -53851,6 +53851,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-linux",
             "value": 359,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d034cbd7ccaf7b073adc980582a9299dedf07854",
+          "message": "fix(deps): bump fast-uri from 3.1.2 to 3.1.4\n\nBumps [fast-uri](https://github.com/fastify/fast-uri) from 3.1.2 to 3.1.4.\n- [Release notes](https://github.com/fastify/fast-uri/releases)\n- [Commits](https://github.com/fastify/fast-uri/compare/v3.1.2...v3.1.4)\n\n---\nupdated-dependencies:\n- dependency-name: fast-uri\n  dependency-version: 3.1.4\n  dependency-type: indirect\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-07-24T10:02:07Z",
+          "tree_id": "6e200e1e9fd6ae72d727ac7080ace58c711b4cf1",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/d034cbd7ccaf7b073adc980582a9299dedf07854"
+        },
+        "date": 1784887377342,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-linux",
+            "value": 2150,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-linux",
+            "value": 1281,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-linux",
+            "value": 276,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-linux",
+            "value": 445,
             "unit": "ms"
           }
         ]
