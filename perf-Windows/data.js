adc74f2ccc5f722275a55922d210f0a7bc50b20e
github-action-benchmark <github@users.noreply.github.com>
2026-08-14 13:54:18 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 1543e5380bf745b975601f29ec972a354fa35bfc

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786715499042,
+  "lastUpdate": 1786715658566,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -55391,6 +55391,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-win32",
             "value": 336,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1543e5380bf745b975601f29ec972a354fa35bfc",
+          "message": "fix(deps): bump @salesforce/core from 9.1.0 to 9.1.1\n\nBumps [@salesforce/core](https://github.com/forcedotcom/sfdx-core) from 9.1.0 to 9.1.1.\n- [Release notes](https://github.com/forcedotcom/sfdx-core/releases)\n- [Changelog](https://github.com/forcedotcom/sfdx-core/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/forcedotcom/sfdx-core/compare/9.1.0...9.1.1)\n\n---\nupdated-dependencies:\n- dependency-name: \"@salesforce/core\"\n  dependency-version: 9.1.1\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-08-14T13:48:41Z",
+          "tree_id": "691f434e6b408ae4f07e75a13a96916a28059ba6",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/1543e5380bf745b975601f29ec972a354fa35bfc"
+        },
+        "date": 1786715649077,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-win32",
+            "value": 3321,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-win32",
+            "value": 2096,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-win32",
+            "value": 326,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-win32",
+            "value": 405,
             "unit": "ms"
           }
         ]
