cc2576182c05ba4b516273e1fcbdc2eacfddbc75
github-action-benchmark <github@users.noreply.github.com>
2026-08-26 00:33:22 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for a24daa6cf29fb1fc221fc8f08f0a917b5adb21ed

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787704223732,
+  "lastUpdate": 1787704402638,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -57019,6 +57019,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-win32",
             "value": 433,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a24daa6cf29fb1fc221fc8f08f0a917b5adb21ed",
+          "message": "fix(deps): bump @salesforce/source-deploy-retrieve from 13.2.0 to 13.3.0\n\nBumps [@salesforce/source-deploy-retrieve](https://github.com/forcedotcom/source-deploy-retrieve) from 13.2.0 to 13.3.0.\n- [Release notes](https://github.com/forcedotcom/source-deploy-retrieve/releases)\n- [Changelog](https://github.com/forcedotcom/source-deploy-retrieve/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/forcedotcom/source-deploy-retrieve/compare/13.2.0...13.3.0)\n\n---\nupdated-dependencies:\n- dependency-name: \"@salesforce/source-deploy-retrieve\"\n  dependency-version: 13.2.3\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-08-26T00:28:32Z",
+          "tree_id": "987d3dbf8fc164817d66a7cbc22e64883a894040",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/a24daa6cf29fb1fc221fc8f08f0a917b5adb21ed"
+        },
+        "date": 1787704380455,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-win32",
+            "value": 2779,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-win32",
+            "value": 1783,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-win32",
+            "value": 262,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-win32",
+            "value": 328,
             "unit": "ms"
           }
         ]
