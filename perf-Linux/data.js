50c0eeba209cfa5bcd47dc3d3f1c73a94217d3b3
github-action-benchmark <github@users.noreply.github.com>
2026-09-12 20:04:16 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for e81bb7309aaacd9e292e133c3b698feb8e89d7ba

--- perf-Linux/data.js
+++ perf-Linux/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788726844443,
+  "lastUpdate": 1789243456530,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -58867,6 +58867,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-linux",
             "value": 208,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e81bb7309aaacd9e292e133c3b698feb8e89d7ba",
+          "message": "chore(deps): bump github/codeql-action from 4.37.9 to 4.38.0\n\nBumps [github/codeql-action](https://github.com/github/codeql-action) from 4.37.9 to 4.38.0.\n- [Release notes](https://github.com/github/codeql-action/releases)\n- [Changelog](https://github.com/github/codeql-action/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/github/codeql-action/compare/v4.37.9...v4.38.0)\n\n---\nupdated-dependencies:\n- dependency-name: github/codeql-action\n  dependency-version: 4.38.0\n  dependency-type: direct:production\n  update-type: version-update:semver-minor\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-09-12T20:03:27Z",
+          "tree_id": "d5f6f1ca0d3cf61fa66ddcccd3ef690dacfe76d0",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/e81bb7309aaacd9e292e133c3b698feb8e89d7ba"
+        },
+        "date": 1789243449510,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-linux",
+            "value": 1974,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-linux",
+            "value": 1112,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-linux",
+            "value": 261,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-linux",
+            "value": 270,
             "unit": "ms"
           }
         ]
