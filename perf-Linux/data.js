a809ed3ea7d1c4de477493350069e485248e6400
github-action-benchmark <github@users.noreply.github.com>
2026-09-18 20:23:26 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 17819fced9065fe91509f6c60b944437013367f4

--- perf-Linux/data.js
+++ perf-Linux/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789762851753,
+  "lastUpdate": 1789763006863,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -59351,6 +59351,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-linux",
             "value": 289,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "17819fced9065fe91509f6c60b944437013367f4",
+          "message": "chore(deps-dev): bump mocha from 12.0.0 to 12.0.2\n\nBumps [mocha](https://github.com/mochajs/mocha) from 12.0.0 to 12.0.2.\n- [Release notes](https://github.com/mochajs/mocha/releases)\n- [Changelog](https://github.com/mochajs/mocha/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/mochajs/mocha/compare/v12.0.0...v12.0.2)\n\n---\nupdated-dependencies:\n- dependency-name: mocha\n  dependency-version: 12.0.2\n  dependency-type: direct:development\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-09-18T20:19:48Z",
+          "tree_id": "1b0fa7535657f4908353a0bb89993d16875d19e2",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/17819fced9065fe91509f6c60b944437013367f4"
+        },
+        "date": 1789763000931,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-linux",
+            "value": 1096,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-linux",
+            "value": 641,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-linux",
+            "value": 109,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-linux",
+            "value": 149,
             "unit": "ms"
           }
         ]
