331c5028b850b46460a91eb8178fd75b1a7d653a
github-action-benchmark <github@users.noreply.github.com>
2026-09-18 20:10:25 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 0b7234c760886e8fa9443326ffccec102e9b136e

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789762090875,
+  "lastUpdate": 1789762225745,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -59263,6 +59263,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-win32",
             "value": 421,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0b7234c760886e8fa9443326ffccec102e9b136e",
+          "message": "chore(deps-dev): bump prettier from 3.9.6 to 3.9.8\n\nBumps [prettier](https://github.com/prettier/prettier) from 3.9.6 to 3.9.8.\n- [Release notes](https://github.com/prettier/prettier/releases)\n- [Changelog](https://github.com/prettier/prettier/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/prettier/prettier/compare/3.9.6...3.9.8)\n\n---\nupdated-dependencies:\n- dependency-name: prettier\n  dependency-version: 3.9.8\n  dependency-type: direct:development\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-09-18T20:06:52Z",
+          "tree_id": "57f177a684a1f974a40e7f8641fe65e22e7209ab",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/0b7234c760886e8fa9443326ffccec102e9b136e"
+        },
+        "date": 1789762217220,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-win32",
+            "value": 2688,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-win32",
+            "value": 1680,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-win32",
+            "value": 255,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-win32",
+            "value": 323,
             "unit": "ms"
           }
         ]
