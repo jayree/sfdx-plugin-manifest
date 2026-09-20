305b02282c2ee1578afa8dd2a9f7bf058b52cb8a
github-action-benchmark <github@users.noreply.github.com>
2026-09-20 20:08:28 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 8be623e4094957eb5690ece2436e5ac3f3cd96ce

--- perf-Linux/data.js
+++ perf-Linux/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789934650891,
+  "lastUpdate": 1789934908238,
   "repoUrl": "https://github.com/jayree/sfdx-plugin-manifest",
   "entries": {
     "Benchmark": [
@@ -59659,6 +59659,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "eda-GitRepo-linux",
             "value": 269,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8be623e4094957eb5690ece2436e5ac3f3cd96ce",
+          "message": "chore(deps-dev): bump eslint from 10.10.0 to 10.11.0\n\nBumps [eslint](https://github.com/eslint/eslint) from 10.10.0 to 10.11.0.\n- [Release notes](https://github.com/eslint/eslint/releases)\n- [Commits](https://github.com/eslint/eslint/compare/v10.10.0...v10.11.0)\n\n---\nupdated-dependencies:\n- dependency-name: eslint\n  dependency-version: 10.11.0\n  dependency-type: direct:development\n  update-type: version-update:semver-minor\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-09-20T20:04:01Z",
+          "tree_id": "eedf55629c5d949087b1c5257293645189d7ff77",
+          "url": "https://github.com/jayree/sfdx-plugin-manifest/commit/8be623e4094957eb5690ece2436e5ac3f3cd96ce"
+        },
+        "date": 1789934901339,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-ComponentSetExtra-linux",
+            "value": 1979,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitDiffResolver-linux",
+            "value": 1119,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-VirtualTreeContainerExtra-linux",
+            "value": 250,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-GitRepo-linux",
+            "value": 283,
             "unit": "ms"
           }
         ]
